929aab569681576239b7c6a080e397620ecf89de
Isara Naranirattisai <isaradream@gmail.com>
2017-05-20 20:02:36 +0000

    feat: Use filesystem (now use lltest)

--- vcowfs.py
+++ vcowfs.py
@@ -6,15 +6,68 @@ from argparse import ArgumentParser
 import subprocess
 import time
 import signal
-import time
+import stat
+import logging
+import errno
 
 mountpoint = ''
 
-def sigint_handler(signum, frame):
-    subprocess.call(["umount", mountpoint])
-    print('Unmounted!!')
-    sys.exit()
+#Code from lltest.py need to implement!!
+class Vcosfs(llfuse.Operations):
+    def __init__(self):
+        super(Vcosfs, self).__init__()
+        self.hello_name = b"Readdir"
+        self.hello_inode = llfuse.ROOT_INODE+1
+        self.hello_data = b"hello world\n"
 
+    def getattr(self, inode, ctx=None):
+        entry = llfuse.EntryAttributes()
+        if inode == llfuse.ROOT_INODE:
+            entry.st_mode = (stat.S_IFDIR | 0o755)
+            entry.st_size = 0
+        elif inode == self.hello_inode:
+            entry.st_mode = (stat.S_IFREG | 0o644)
+            entry.st_size = len(self.hello_data)
+        else:
+            raise llfuse.FUSEError(errno.ENOENT)
+
+        stamp = int(1438467123.985654 * 1e9)
+        entry.st_atime_ns = stamp
+        entry.st_ctime_ns = stamp
+        entry.st_mtime_ns = stamp
+        entry.st_gid = os.getgid()
+        entry.st_uid = os.getuid()
+        entry.st_ino = inode
+
+        return entry
+
+    def lookup(self, parent_inode, name, ctx=None):
+        if parent_inode != llfuse.ROOT_INODE or name != self.hello_name:
+            raise llfuse.FUSEError(errno.ENOENT)
+        return self.getattr(self.hello_inode)
+
+    def opendir(self, inode, ctx):
+        if inode != llfuse.ROOT_INODE:
+            raise llfuse.FUSEError(errno.ENOENT)
+        return inode
+
+    def readdir(self, fh, off):
+        assert fh == llfuse.ROOT_INODE
+        print("ReadDir")
+        # only one entry
+        if off == 0:
+            yield (self.hello_name, self.getattr(self.hello_inode), 1)
+
+    def open(self, inode, flags, ctx):
+        if inode != self.hello_inode:
+            raise llfuse.FUSEError(errno.ENOENT)
+        if flags & os.O_RDWR or flags & os.O_WRONLY:
+            raise llfuse.FUSEError(errno.EPERM)
+        return inode
+
+    def read(self, fh, off, size):
+        assert fh == self.hello_inode
+        return self.hello_data[off:off+size]
 
 def parse_args(args):
     '''Parse command line'''
@@ -29,8 +82,6 @@ def parse_args(args):
                         help='time of versioning period')
     return parser.parse_args(args)
 
-signal.signal(signal.SIGINT, sigint_handler)
-
 
 def main():
     options = parse_args(sys.argv[1:])
@@ -39,9 +90,18 @@ def main():
     global mountpoint
     mountpoint = options.mountpoint
     subprocess.call(["mount", options.image, options.mountpoint])
-    while(1):
-        print('.')
-        time.sleep(1)
+
+    testfs = Vcosfs()
+    fuse_options = set(llfuse.default_options)
+    fuse_options.add('fsname=vcowfs')
+    llfuse.init(testfs, options.mountpoint, fuse_options)
+    try:
+        llfuse.main(workers=1)
+    except:
+        llfuse.close(unmount=False)
+        raise
+
+    llfuse.close()
 
 if __name__ == '__main__':
     main()
